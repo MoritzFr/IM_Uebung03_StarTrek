1be3cc41c47a17d1e7addf5a224b6ee7f5c4ebca
MoritzFr <73073137+MoritzFr@users.noreply.github.com>
2022-05-09 23:33:41 +0200

    last changes

--- script.js
+++ script.js
@@ -5,10 +5,18 @@ function main() {
     const b2 = document.getElementById('b2');
     const p1 = document.getElementById('p1');
     const p2 = document.getElementById('p2');
+    const bitbsp = document.getElementById('bitbsp');
+    const outbsp = document.getElementById('outbsp');
+
+p2.style.display = 'none';
+outbsp.style.display = 'none';
+b1.className = "button disabled"
 
 b1.addEventListener('click', (e) => {
     p1.style.display = 'block';
     p2.style.display = 'none';
+    bitbsp.style.display = 'block';
+    outbsp.style.display = 'none';
     b1.className = "button disabled";
     b2.className = "button";
 })
@@ -16,6 +24,8 @@ b1.addEventListener('click', (e) => {
 b2.addEventListener('click', (e) => {
     p1.style.display = 'none';
     p2.style.display = 'block';
+    bitbsp.style.display = 'none';
+    outbsp.style.display = 'block';
     b2.className = "button disabled";
     b1.className = "button";
 })
